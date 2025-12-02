54be25eb380d7d48a281b3fb4510ad59958f4d16
mbquial <pepitotolitis@gmail.com>
2025-12-01 23:15:52 -0500

    feat: first home page implementation

--- src/App.tsx
+++ src/App.tsx
@@ -6,47 +6,10 @@ import Auth from "@/pages/login/hooks/Auth";
 import { PasswordRecoveryContainer } from "@/pages/password-recovery/passwordRecoveryContainer";
 import { AnimatePresence, motion } from "framer-motion";
 
+import Home from "@/pages/home/Home";
 function App() {
-    const location = useLocation();
-
-    return (
-        <AuthProvider>
-            <AnimatePresence mode="wait">
-                <Routes location={location} key={location.pathname}>
-                    <Route path="/auth" element={<Auth />} />
-                    <Route path="/login" element={<Navigate to="/auth" replace />} />
-                    <Route path="/signup" element={<Navigate to="/auth" replace />} />
-
-                    <Route
-                        path="/forgot-password"
-                        element={
-                            <motion.div
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1 }}
-                                exit={{ opacity: 0 }}
-                                transition={{ duration: 0.5 }}
-                            >
-                                <PasswordRecoveryContainer />
-                            </motion.div>
-                        }
-                    />
-
-                    <Route element={<ProtectedRoute />}>
-                        <Route element={<Layout />}>
-                            <Route path="/catalog" element={<div>Catalog</div>} />
-                            <Route path="/shoppingCart" element={<div>Shopping Cart</div>} />
-                            <Route path="/orders" element={<div>Orders</div>} />
-                            <Route path="/chat" element={<div>Chat</div>} />
-                            <Route path="/dashboard" element={<div>Dashboard</div>} />
-                            <Route path="/" element={<Navigate to="/dashboard" replace />} />
-                        </Route>
-                    </Route>
-
-                    <Route path="*" element={<Navigate to="/login" replace />} />
-                </Routes>
-            </AnimatePresence>
-        </AuthProvider>
-    );
+  return <Home />;
 }
+
 
 export default App;
